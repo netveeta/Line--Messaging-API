20e81ea3d40d1f23172a591057dfe96dfb35249b
jeerapote <jeerapote4@hotmail.com>
2017-06-14 17:31:50 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'xxxxxxxx';
+$access_token = 'NYd8+951SKv8ah9nHcP5HsZ8wltb7H2yyUIL2Cgk+oP5YNI2NIguihcaxAyzfD7Q8JxRR2oBfhTfAW8dJsqxPw90/fBriowKHECtdgsHXP9NsPFN3CX7+H/VU2uRQD65rXiUd4idLNCDAJ+hgAlnLQdB04t89/1O/w1cDnyilFU=';
 
 $baseUrl = "https://ecs.egat.co.th/public.php?service=files&t=fd20b4335410e38c017713bd6d458deb";
 
