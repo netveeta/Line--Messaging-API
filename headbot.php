acd3b6f6be91c79b530752fe6c53bf12655fdfda
jeerapote <jeerapote4@hotmail.com>
2017-06-14 17:15:12 +0700

    Create headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'juzbEAu+T7V0MLnZZevyeLYtSBGX0WGpS8Dcq7SPHFZvuWkT9FKm3NYrSk/ED8sP0SBK4u1c8pZyFSh5+geh2jDka8GI7pPh0DtIBGqeAgj5W9Gx3e/0+A/p66+EHMixoQVE1NH5FeNEpbdIzAe3NQdB04t89/1O/w1cDnyilFU=';
+$access_token = 'xxxxxxxx';
 
 $baseUrl = "https://ecs.egat.co.th/public.php?service=files&t=fd20b4335410e38c017713bd6d458deb";
 
