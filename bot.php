a261ee267b81742b962471bf55ecd4a8b8608dbf
jeerapote <jeerapote4@hotmail.com>
2018-06-09 16:56:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,15 +28,12 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-
-	
-			
+		  
 			$result = curl_exec($ch);
 			curl_close($ch);
-
 			echo $result . "\r\n";
 			
-	  }
+	  }// End Reply userID
 		if ($event['type'] == 'join') {
 			// Get user follow or join me
 			$touserid = $event['source']['userId'];
