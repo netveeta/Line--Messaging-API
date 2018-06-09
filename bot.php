7103498906d500b62a1d139d59d0fa1ecdd8316f
jeerapote <jeerapote4@hotmail.com>
2018-06-09 16:54:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,32 @@ if (!is_null($events['events'])) {
   foreach ($events['events'] as $event) {
     // Reply only when Follow me.
 	  if ($event['type'] == 'message'){
-		  
+		  $gentext = $event['source']['userId'];
+			//$gentext = "ขอบคุณที่ติดตามเรา";
+			// Get Replytoken
+			$replyToken = $event['replyToken'];
+			//Make a POST Request to Messaging API to reply to follower
+			//$messages = t1($togroupid);
+		        $messages = t1($gentext);
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = data1($replyToken,$messages);
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+	
+			
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+			
 	  }
 		if ($event['type'] == 'join') {
 			// Get user follow or join me
