837812adc8695682de0052b9181856e892f6838c
jeerapote <jeerapote4@hotmail.com>
2018-06-09 16:42:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,10 +35,7 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
+	
 			
 			$gentext1 = $event['source']['userId'];
 			//$gentext = "ขอบคุณที่ติดตามเรา";
