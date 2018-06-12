6091acb2bb9a46b399aec82ad94b66a59bd803b4
jeerapote <jeerapote4@hotmail.com>
2018-06-12 10:19:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,7 +18,8 @@ if (!is_null($events['events'])) {
 			//Make a POST Request to Messaging API to reply to follower
 			//$messages = t1($togroupid);
 		        $messages = t1($gentext);
-			$url = 'https://api.line.me/v2/bot/message/reply';
+			//$url = 'https://api.line.me/v2/bot/message/reply';
+		        $url = 'https://api.line.me/v2/bot/message/push';
 			$data = data1($replyToken,$messages);
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
