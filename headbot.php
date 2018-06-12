2f1cdaf40403850abb481617d4349af824bb61b3
jeerapote <jeerapote4@hotmail.com>
2018-06-12 10:00:47 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'xxxx';
+$access_token = 'vIqVV9lNX5yNkf7r4nm+FFAesNeaypSuYC/OOW9LOiRptDrt0/ELtOJekuhmabamyn5ssrtDODisO/XE2wvauE7MTr1C0xIY84aHbRZRQDRtEojxs7UtkvssK7Y4eS4Xj/r+krB7u9ueoZVAjmOvMwdB04t89/1O/w1cDnyilFU=';
 
 $baseUrl = "xxxxx";
 
