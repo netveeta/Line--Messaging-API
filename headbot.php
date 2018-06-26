b635fc06f05b4f27ce6cf3990f3f650d6dc7ec41
netveeta <40591983+netveeta@users.noreply.github.com>
2018-06-26 14:58:28 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -1,6 +1,6 @@
 <?php
 
-$access_token = 'vIqVV9lNX5yNkf7r4nm+FFAesNeaypSuYC/OOW9LOiRptDrt0/ELtOJekuhmabamyn5ssrtDODisO/XE2wvauE7MTr1C0xIY84aHbRZRQDRtEojxs7UtkvssK7Y4eS4Xj/r+krB7u9ueoZVAjmOvMwdB04t89/1O/w1cDnyilFU=';
+$access_token = 'oQP+9trIj8cu5euYApwjCUuhjIYAGWFqBCxH+xq4nV+r7srgSg/Kz5zqJTLnMi1Gh8RGyw6eMw7jFdfs2QpVgw1hKhd1qesIKE7wYNbD3hHwBbhCGrROslw956wE4nUbWAtpHn45N2zkva4oiWkMggdB04t89/1O/w1cDnyilFU=';
 
 $baseUrl = "xxxxx";
 
